5aa6e9c846e1e5c6469122f0aca80f9a1c2cbdf2
Ruben Koster <rkoster@starkandwayne.com>
2017-05-02 16:58:41 +0200

    Define out params for run-errand resource

--- concourse/out_params.go
+++ concourse/out_params.go
@@ -1,12 +1,7 @@
 package concourse
 
 type OutParams struct {
-	Manifest  string                 `json:"manifest"`
-	NoRedact  bool                   `json:"no_redact,omitempty"`
-	Cleanup   bool                   `json:"cleanup,omitempty"`
-	Releases  []string               `json:"releases,omitempty"`
-	Stemcells []string               `json:"stemcells,omitempty"`
-	Vars      map[string]interface{} `json:"vars,omitempty"`
-	VarsFiles []string               `json:"vars_files,omitempty"`
-	OpsFiles  []string               `json:"ops_files,omitempty"`
+	ErrandName  string `json:"name,omitempty"`
+	KeepAlive   bool   `json:"keep_alive,omitempty"`
+	WhenChanged bool   `json:"when_changed,omitempty"`
 }
