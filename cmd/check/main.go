8f481017dcc0ee98d2824394f8fc891911660981
Ruben Koster <rkoster@starkandwayne.com>
2017-05-03 15:19:34 +0200

    Revert "Remove check and in cmds"
    This reverts commit 8e1f5b1b58ebbbb8a3e83fe88d8ab65f8d8ce4ff.

---
+++ cmd/check/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"io/ioutil"
+
+	"github.com/starkandwayne/bosh2-errand-resource/bosh"
+	"github.com/starkandwayne/bosh2-errand-resource/check"
+	"github.com/starkandwayne/bosh2-errand-resource/concourse"
+)
+
+func main() {
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot read configuration: %s\n", err)
+		os.Exit(1)
+	}
+
+	checkRequest, err := concourse.NewCheckRequest(stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Invalid parameters: %s\n", err)
+		os.Exit(1)
+	}
+
+	cliCoordinator := bosh.NewCLICoordinator(checkRequest.Source, os.Stderr)
+	commandRunner := bosh.NewCommandRunner(cliCoordinator)
+	cliDirector, err := cliCoordinator.Director()
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	director := bosh.NewBoshDirector(checkRequest.Source, commandRunner, cliDirector)
+
+	checkCommand := check.NewCheckCommand(director)
+	checkResponse, err := checkCommand.Run(checkRequest)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	concourseOutputFormatted, err := json.MarshalIndent(checkResponse, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not generate version: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s", concourseOutputFormatted)
+}
