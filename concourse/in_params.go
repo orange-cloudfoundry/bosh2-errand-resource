c03779b589197b9ea6630ebfa3f9b2a72a15f7e3
Ruben Koster <rkoster@starkandwayne.com>
2017-05-03 15:33:04 +0200

    Remove compiled releases download from in command

--- concourse/in_params.go
+++ concourse/in_params.go
@@ -1,9 +1,4 @@
 package concourse
 
-type CompiledRelease struct {
-	Name string `json:"name"`
-}
-
 type InParams struct {
-	CompiledReleases []CompiledRelease `json:"compiled_releases,omitempty"`
 }
