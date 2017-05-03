6f2bcb3fcd7d896091ddb931dd4e2ff8cb4f7b49
Ruben Koster <rkoster@starkandwayne.com>
2017-05-03 10:15:55 +0200

    Fix repo name in build script

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -7,7 +7,7 @@ export GOPATH="${PWD}/gopath"
 export PATH="${GOPATH}/bin:${PATH}"
 go get github.com/onsi/ginkgo/ginkgo
 
-cd gopath/src/github.com/cloudfoundry/bosh-deployment-resource
+cd gopath/src/github.com/starkandwayne/bosh2-errand-resource
 
 go build -o "$BUILT_BINARIES_DIR/out" ./cmd/out
 
